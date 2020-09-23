850f218ddc1c8cbbe04ba01f046c91d735a7b163
CeciliaDeschenes <71787279+CeciliaDeschenes@users.noreply.github.com>
2020-09-23 16:36:44 -0400

    Add files via upload

---
+++ Download Ch 2 data.R
@@ -0,0 +1,15 @@
+
+rm(list=ls())
+
+working_directory <- getwd()
+download.file("https://github.com/CeciliaDeschenes/BUS211.Data/raw/master/Ch2Data.RData",
+              "Ch2Data.RData")
+load(file.path(working_directory, "Ch2Data.RData"))
+
+# idea: create function to do this dynamically
+# getdata <- function(input.name){
+#   working_directory <- getwd()
+#   download.file("https://github.com/CeciliaDeschenes/BUS211.Data/raw/master/Ch2Data.RData",
+#                 "Ch2Data.RData")
+#   load(file.path(working_directory, "Ch2Data.RData"))
+# }
